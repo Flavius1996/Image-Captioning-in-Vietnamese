7aabfbcd31ed2258379b1b692290b5b1294bb029
Flavius1996 <Flavius1996@users.noreply.github.com>
2018-07-13 22:00:24 +0700

    Uncomment download code

--- down_inceptionv3.sh
+++ down_inceptionv3.sh
@@ -2,6 +2,6 @@
 INCEPTION_DIR="${HOME}/im2txt/data"
 mkdir -p ${INCEPTION_DIR}
 
-#wget "http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz"
+wget "http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz"
 tar -xvf "inception_v3_2016_08_28.tar.gz" -C ${INCEPTION_DIR}
-#rm "inception_v3_2016_08_28.tar.gz"
+rm "inception_v3_2016_08_28.tar.gz"
